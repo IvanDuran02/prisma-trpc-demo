6f7ae6b43cdc095ecb4f012a60fd76103b52abac
Ivan Duran <iamivanduran02@gmail.com>
2022-08-20 15:15:01 -0400

    added trpc with an example query from the docs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,43 @@
 import "../styles/globals.css";
+import { withTRPC } from "@trpc/next";
 import type { AppProps } from "next/app";
+import { AppRouter } from "./api/trpc/[trpc]";
+
+// The createReactQueryHooks function expects certain parameters to be passed via the Context API. To set these parameters
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+  // reference for vercel.com
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`; // incase process.env.PORT is not defined run on 3000 by default
+}
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    return {
+      url: `${getBaseUrl()}/api/trpc`,
+      /**
+       * @link https://react-query-v3.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
